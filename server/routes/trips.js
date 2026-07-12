29523dbc7e6d4134f98dc4812d72b9dbaf7d26af
prince-dungrani <princedungrani30@gmail.com>
2026-07-12 14:35:59 +0530

    feat(server): update trip routes to allow financial analysts to view trip records

--- server/routes/trips.js
+++ server/routes/trips.js
@@ -5,8 +5,8 @@ const { authenticate, authorize } = require('../middleware/auth');
 router.use(authenticate);
 
 // dispatcher edits trips; safety_officer can view
-router.get('/', authorize('dispatcher', 'safety_officer', 'fleet_manager'), getTrips);
-router.get('/:id', authorize('dispatcher', 'safety_officer', 'fleet_manager'), getTrip);
+router.get('/', authorize('dispatcher', 'safety_officer', 'fleet_manager', 'financial_analyst'), getTrips);
+router.get('/:id', authorize('dispatcher', 'safety_officer', 'fleet_manager', 'financial_analyst'), getTrip);
 router.post('/', authorize('dispatcher'), createTrip);
 router.patch('/:id/dispatch', authorize('dispatcher'), dispatchTrip);
 router.patch('/:id/complete', authorize('dispatcher'), completeTrip);
